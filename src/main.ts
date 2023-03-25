559aecb95e26ab02c9a891cc83a6dd68f9828b00
Daniel Sykora <Daniel.Sykora5@gmail.com>
2023-03-25 16:29:37 +0100

    PWA support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './app.css'
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 const app = new App({
   target: document.getElementById('app')
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    preprocess: vitePreprocess()
-  })]
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess()
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}']
+      },
+      manifest: {
+        name: "",
+        short_name: "",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone"
+      }
+    })
+  ]
 })
